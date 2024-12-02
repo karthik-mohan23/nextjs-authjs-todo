05ad17c1a1c058f82b2628ef42a03df7f43fca77
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-12-02 14:49:35 +0530

    setup neon db

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,17 @@
+import ws from "ws";
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  neonConfig.webSocketConstructor = ws;
+  const connectionString = `${process.env.DATABASE_URL}`;
+
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaNeon(pool);
+  const prisma = new PrismaClient({ adapter });
+
+  return prisma;
 };
 
 declare const globalThis: {
